435dd5030ecbb291d082a774c3ff0a3afeef3fb7
Mateusz Kozlowsk <climatik@world.pl>
2022-05-17 20:07:08 +0200

    create new files structure in new repository

---
+++ 1.Fundementals Part 1/main.js
@@ -0,0 +1,17 @@
+let years  = new Date().getFullYear();
+console.log(years);
+
+let birthyears = 1990;
+
+console.log(years - birthyears);
+
+let array = [1,2,3,4,5,6,7,8,9];
+console.log(array)
+let arr2 = [...array];
+console.log(arr2)
+let arr3 = array;
+console.log(arr3);
+
+array.splice(0,3);
+console.log(array)
+
