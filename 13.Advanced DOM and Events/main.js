eb130b4bca1e75343365f6a55fbfc11f8ca24e65
Mateusz Kozlowsk <climatik@world.pl>
2022-07-11 11:58:08 +0200

    advanded DOM and events

--- 13.Advanced DOM and Events/main.js
+++ 13.Advanced DOM and Events/main.js
@@ -10,40 +10,31 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollsTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1')
 
-
+//open modal
 const openModal = function (e) {
     e.preventDefault()
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 };
-
+//close modal
 const closeModal = function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 };
-btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal))
 
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal))
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
-
 document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
         closeModal();
     }
 });
 
+//BTN scrolling
+//BTN scrolling
 btnScrollsTo.addEventListener('click', function (e) {
-    const s1coords = section1.getBoundingClientRect();
-    console.log(s1coords)
-    console.log(e.target.getBoundingClientRect())
-    console.log('Current scroll X/Y', window.pageXOffset, window.pageYOffset);
-
-    //scrolling
-    // window.scrollTo(
-    //     s1coords.left + window.pageXOffset,
-    //     s1coords.top + window.pageYOffset
-    // );
-
+    // const s1coords = section1.getBoundingClientRect();
 //oldschool way to scrolling page
 //     window.scrollTo({
 //         left: s1coords.left + window.pageXOffset,
@@ -55,17 +46,30 @@ btnScrollsTo.addEventListener('click', function (e) {
     section1.scrollIntoView({behavior: 'smooth'})
 })
 
-const h1 = document.querySelector('h1');
+//PAGE NAVIGATION;///////////////////////////
 
-const alertH1 = function (e) {
-    alert('onmouseenter: Great you are reading the heading');
+// document.querySelectorAll('.nav__link')
+//     .forEach(item =>
+//         item.addEventListener('click', function (e) {
+//             e.preventDefault();
+//             const id = this.getAttribute('href');
+//             console.log(id);
+//             const section = document.querySelector(id);
+//             section.scrollIntoView({behavior: 'smooth'})
+//
+//         }))
 
-    h1.removeEventListener('mouseenter', alertH1)
-};
+//1. Add event listener to common parent element.
+//2. Determine what element orginated the event
 
-h1.addEventListener('mouseenter', alertH1);
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+    console.log(e.target);
+    e.preventDefault();
 
-// h1.onmouseenter = function (e) {
-//     alert('onmouseenter: Great you are reading the heading')
-// };
-
+    //matching strategy
+    if (e.target.classList.contains('nav__link')) {
+        const id = e.target.getAttribute('href');
+        const section = document.querySelector(id);
+        section.scrollIntoView({behavior: 'smooth'});
+    }
+})
