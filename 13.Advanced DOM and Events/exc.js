9bc5917a6c98d541bddef1061dbe12b417bfa829
Mateusz Kozlowsk <climatik@world.pl>
2022-07-11 11:11:27 +0200

    advanded DOM and events

--- 13.Advanced DOM and Events/exc.js
+++ 13.Advanced DOM and Events/exc.js
@@ -1,24 +1,45 @@
 'use strict'
 
-console.log(document.documentElement)
-console.log(document.head)
-console.log(document.body)
+// console.log(document.documentElement)
+// console.log(document.head)
+// console.log(document.body)
 
-const header = document.querySelector('.header')
-const allSections = document.querySelectorAll('.section');
-console.log(allSections)
+// const header = document.querySelector('.header')
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections)
 
-const message = document.createElement('div');
-message.classList.add('cookie-message');
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
 // message.textContent = 'We use cookied for improved functionality and analytics';
-message.innerHTML = 'We use cookied for improved functionality and analytis <button class="btn btn--close-cookie">Got it!</button>';
-
+// message.innerHTML = 'We use cookied for improved functionality and analytis <button class="btn btn--close-cookie">Got it!</button>';
 
 // header.prepend(message)
-header.append(message)
+// header.append(message)
 // header.append(message.cloneNode(true))
 
 //Delete elements
-document.querySelector('.btn--close-cookie').addEventListener('click', function (){
-    message.remove()
-})
+// document.querySelector('.btn--close-cookie').addEventListener('click', function (){
+//     message.remove()
+// })
+
+//styles
+//setProperty
+// message.style.backgroundColor = '#37383d'
+// document.documentElement.style.setProperty('--color-primary', 'tomato')
+//
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt)
+// console.log(logo.src)
+// console.log(logo.className);
+// logo.alt = 'Beatiful minimalist logo';
+// console.log(logo.alt)
+
+//
+// logo.setAttribute('company', 'Banklist');
+//
+//
+// //classes
+// logo.classList.add('c')
+// logo.classList.remove('c')
+// logo.classList.toggle('c')
+// logo.classList.contains('c')
--- 13.Advanced DOM and Events/main.js
+++ 13.Advanced DOM and Events/main.js
@@ -7,6 +7,9 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollsTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1')
+
 
 const openModal = function (e) {
     e.preventDefault()
@@ -28,3 +31,41 @@ document.addEventListener('keydown', function (e) {
         closeModal();
     }
 });
+
+btnScrollsTo.addEventListener('click', function (e) {
+    const s1coords = section1.getBoundingClientRect();
+    console.log(s1coords)
+    console.log(e.target.getBoundingClientRect())
+    console.log('Current scroll X/Y', window.pageXOffset, window.pageYOffset);
+
+    //scrolling
+    // window.scrollTo(
+    //     s1coords.left + window.pageXOffset,
+    //     s1coords.top + window.pageYOffset
+    // );
+
+//oldschool way to scrolling page
+//     window.scrollTo({
+//         left: s1coords.left + window.pageXOffset,
+//         top: s1coords.top + window.pageYOffset,
+//         behavior: 'smooth',
+//     });
+
+    //modern way ;
+    section1.scrollIntoView({behavior: 'smooth'})
+})
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+    alert('onmouseenter: Great you are reading the heading');
+
+    h1.removeEventListener('mouseenter', alertH1)
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+// h1.onmouseenter = function (e) {
+//     alert('onmouseenter: Great you are reading the heading')
+// };
+
