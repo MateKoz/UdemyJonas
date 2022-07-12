adaf490cdd7d5d696a9af6e9d46cad4887f16e00
Mateusz Kozlowsk <climatik@world.pl>
2022-07-12 17:26:26 +0200

    oop js

--- 14. Object Oriented Programming/exc.js
+++ 14. Object Oriented Programming/exc.js
@@ -1,26 +1,38 @@
-const Car = function (make, speed) {
-    this.make = make;
-    this.speed = speed;
+class Car {
+    constructor(make, speed) {
+        this.make = make;
+        this.speed = speed
+    }
+
+    accelerate() {
+        this.speed += 10
+        console.log(`${this.make} accelerate by 10 km/h, now is ${this.speed}`)
+    }
+
+    break() {
+        this.speed -= 5
+        console.log(`${this.make} break now by 10km/h less, now is ${this.speed}`)
+    }
+
+    get speedUS() {
+        return this.speed / 1.6
+    }
+
+    set speedUS(speed) {
+        this.speed = speed * 1.6;
+    }
 }
 
 const bmw = new Car('BMW', 120);
 const mercedes = new Car('Mercedes', 95);
 
-Car.prototype.accelerate = function () {
-    this.speed+=10
-    console.log(`${this.make} accelerate by 10 km/h, now is ${this.speed}`)
-}
-
-Car.prototype.break = function () {
-    this.speed -=5
-    console.log(`${this.make} break now by 10km/h less, now is ${this.speed}`)
-}
-
-// bmw.accelerate();
-// mercedes.accelerate();
-//
-// bmw.break()
-// mercedes.break()
-// mercedes.break()
-// mercedes.break()
-// mercedes.break()
+const ford = new Car('Ford', 120);
+console.log('=================')
+console.log(ford.speedUS);
+ford.accelerate()
+ford.accelerate()
+ford.accelerate()
+ford.accelerate()
+console.log(ford.speedUS);
+ford.speedUS = 50;
+console.log(ford)
