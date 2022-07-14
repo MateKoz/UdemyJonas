97716f82f9b9977f215e07f4795ce06f678decbb
Mateusz Kozlowsk <climatik@world.pl>
2022-07-14 15:32:43 +0200

    ajax, apis

--- 16.Asynchronous JS Promises, AJAX annd APIs/script.js
+++ 16.Asynchronous JS Promises, AJAX annd APIs/script.js
@@ -60,29 +60,39 @@
 //             countriesContainer.style.padding = 10 + `px`
 //         })
 // };
-// btn.addEventListener('click', function () {
-// //     getCountryData('poland')
-// // })
-// //
+// // btn.addEventListener('click', function () {
+// // //     getCountryData('poland')
+// // // })
+// // //
 //
-// const whereAmI = function (lat, lng) {
-//     fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-//         .then(res => {
-//             if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
-//             return res.json();
-//         })
-//         .then(data => {
-//             console.log(data);
-//             console.log(`You are in ${data.city}, ${data.country}`);
-//             return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
-//         })
-//         .then(res => {
-//             if (!res.ok) throw new Error(`Country not found (${res.status})`);
-//             return res.json();
-//         })
-//         .then(data => renderCountry(data[0]))
-//         .catch(err => console.error(`${err.message} 💥`));
-// };
-// whereAmI(52.508, 13.381);
-// whereAmI(19.037, 72.873);
-// // whereAmI(-33.933, 18.474);
+// // const whereAmI = function (lat, lng) {
+// //         fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+// //             .then(res => {
+// //                 if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+// //                 return res.json();
+// //             })
+// //             .then(data => {
+// //                 console.log(data);
+// //                 console.log(`You are in ${data.city}, ${data.country}`);
+// //                 return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+// //             })
+// //             .then(res => {
+// //                 if (!res.ok) throw new Error(`Country not found (${res.status})`);
+// //                 return res.json();
+// //             })
+// //             .then(data => renderCountry(data[0]))
+// //             .catch(err => console.error(`${err.message} 💥`));
+// //     }
+// // ;
+// // whereAmI(52.508, 13.381);
+// // whereAmI(19.037, 72.873);
+// // // whereAmI(-33.933, 18.474);
+
+const whereAmI = async function (country) {
+    const res = await fetch(`https://restcountries.eu/rest/v2/name/${country}`);
+    console.log(res);
+    const data = await res.json();
+    console.log(data);
+}
+whereAmI('portugal');
+console.log('FIRST');
